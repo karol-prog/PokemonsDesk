3d2f2521ea820be9e0d513b42ee0eae5c2f43270
Karol Michalik <bel1nqone@gmail.com>
2024-04-01 19:37:48 +0200

    update
    displaying details of each pokemon,
    fetch the api for details of each api,
    styling the detail div

--- public/app.js
+++ public/app.js
@@ -7,7 +7,6 @@ async function getPokemon() {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const response = await fetch(`${url}`);
     const data = await response.json();
-    /* console.log(data); */
     //push the data to array
     allPromises.push(data);
 
@@ -18,7 +17,7 @@ async function getPokemon() {
       image: data.sprites.front_default,
       types: data.types.map((type) => type.type.name).join(", "),
     }));
-    console.log(allPokemon);
+    /* console.log(allPokemon); */
     renderPokemons(allPokemon);
   }
 }
@@ -31,7 +30,7 @@ function renderPokemons(pokemon) {
 
   pokemon.forEach((poke) => {
     pokemonToHtml += `
-      <li class=" bg-red-800  flex flex-col justify-center align-center text-center rounded-xl hover:shadow-md">
+      <li class="hover:cursor-pointer bg-red-800  flex flex-col justify-center align-center text-center rounded-xl hover:shadow-md" onclick="getDetailsPokemons(${poke.id})">
         <img src=${poke.image} class="bg-cover bg-center bg-no-repeat mx-auto w-3/6">
         <div class="p-4">
           <h1 class="uppercase font-bold text-xl">${poke.id}. ${poke.name}</h1>
@@ -40,6 +39,43 @@ function renderPokemons(pokemon) {
       </li>
     `;
   });
-
   pokeGrid.innerHTML = pokemonToHtml;
+}
+
+//function onclick for getting details of each pokemon
+async function getDetailsPokemons(pokemonId) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+  const response = await fetch(`${url}`);
+  const data = await response.json();
+  //call the function for renderDetails
+  renderDetails(data);
+}
+
+//function for displaying details of each pokemon
+function renderDetails(detail) {
+  const abilities = detail.abilities.map((ability) => {
+    return ability.ability.name;
+  });
+
+  const htmlDetail = `
+    <div id="details" class="fixed top-0 left-0 bg-orange-700 w-screen h-screen opacity-95">
+      <button id="close-btn" class="border-none rounded-xl bg-red-800 p-3 absolute top-10 right-10 hover:cursor-pointer" onclick="closeDetails()">Close</button>
+
+      <div class="bg-red-800 max-w-xl flex flex-col justify-center align-center text-center rounded-xl mx-auto my-40">
+        <img src=${detail.sprites.front_default} class="bg-cover bg-center bg-no-repeat mx-auto w-3/6">
+        <div class="p-4">
+          <h1 class="uppercase font-bold text-xl">${detail.id}. ${detail.name}</h1>
+           <p><small>Height: ${detail.height} kg</small> | <small>Weight: ${detail.weight} kg</small></p> 
+           <p><small>Abilities: ${abilities}</small></p>
+        </div>
+      </div>
+    </div>
+  `;
+  pokeGrid.innerHTML = htmlDetail + pokeGrid.innerHTML;
+}
+
+//funciton onclick for closing the details
+function closeDetails() {
+  const details = document.getElementById("details");
+  details.parentElement.removeChild(details);
 }
